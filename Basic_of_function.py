3d76a51f621cc00ca8d86dc5cfce2f758157c232
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-09 14:48:35 +0000

    Add returning values

--- Basic_of_function.py
+++ Basic_of_function.py
@@ -12,6 +12,12 @@ def set_num():
     num = 5 #Creating new variable num inside this functions
     char = 'a'
     
+def sum_of_two_numbers(num_1,num_2):
+    return num_1+num_2
+
+def multiply_two_numbers(num_1,num_2):
+    return num_1*num_2
+    
     
 #main function    
 print_favourite_movie("Title")  #calling function
@@ -21,3 +27,6 @@ print("Num before set_num(): {}".format(num))
 set_num()
 #in function set_num() variable named num exist only in this function, function do not have acces to global variable
 print("Num after set_num(): {}".format(num))
+
+print(sum_of_two_numbers(2,2))
+print(multiply_two_numbers(3,23))
