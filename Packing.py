81a30aa4e2c1b3a30b795e81961ef053fdaaf237
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-09 15:46:21 +0000

    Add unpacking example

--- Packing.py
+++ Packing.py
@@ -13,11 +13,27 @@ def calculate_total(*args):
           total += args[i]
     return total
  
-    
 #Unpacking    
+def unpacker():
+    return(1,2,30)
 
+def separate_info(name):
+    list = name.split(" ")
+    return list
+
+
+           
 #main program
 packer("Hello","my","name","is","John","Cena")
 total = calculate_total(12,32,55,33,21,4,23,64,12,44,32,1,1,1)
-print("\n{}".format(total))
-                   
+print("\n{}\n".format(total))
+v1,v2,v3 = unpacker()
+print(unpacker())
+print(v1)
+print(v2)
+print(v3)
+
+full_name = input("Please enter your full name: \n")
+user_name, user_surname = separate_info(full_name)
+print(user_name)
+print(user_surname)
